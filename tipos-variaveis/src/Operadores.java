bf99c62b2c56f27455e05367f38b903762c499a8
Afonso Júnior <ti.afonsojunior@gmail.com>
2023-09-18 21:51:36 -0300

    Operadores commit1

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,11 @@
+public class Operadores {
+    
+    public static void main(String[] args){
+
+        String nome = "LINGUAGEM";
+        String sobrenome = "JAVA";
+
+        System.out.println(nome + sobrenome);
+
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500; // atribuir valor de milhar
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; // cast = Pega um tipo mais abragente e converte para um tipo mais especifico(que é o correspondente)
+
+        final double VALOR_DE_PI = 3.14; // Toda constante deve conter a palavra reservada "final" e ser escrita em CAIXA ALTA.
+
+        
+
     }
+
 }
