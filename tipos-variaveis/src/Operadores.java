616528fea4aaa8cbd0979d02bc2e0bbd0c590ba6
Afonso Júnior <ti.afonsojunior@gmail.com>
2023-09-20 18:06:44 -0300

    Operadores-finalizado

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -2,10 +2,128 @@ public class Operadores {
     
     public static void main(String[] args){
 
-        String nome = "LINGUAGEM";
+       /*  String nome = "LINGUAGEM";
         String sobrenome = "JAVA";
 
         System.out.println(nome + sobrenome);
 
-    }
+        // Negando um número
+        
+        int numero = 5;
+
+        numero = - numero;
+
+        System.out.println(numero);
+
+        // Tornando o número positivo novamente 
+
+        numero = numero * -1;
+
+        System.out.println(numero);
+
+        // Incrementado valores em 1 unidade
+
+        int numero = 5;
+
+        numero++;
+
+        // Ou assim:
+    
+        System.out.println(++ numero);
+
+        // Decrementando 
+
+        int numero = 5;
+
+        numero--;
+
+        // Ou assim:
+        
+        System.out.println(numero --);
+
+        //Invertendo o valor de expressões booleanas
+
+        boolean variavel = true;
+
+        variavel = !variavel;
+
+        
+         //EXEMPLODE CONDICIONAL UTILIZANDO A ESTRUTURA IF/ELSE
+         int a, b;
+
+         a = 5;
+         b = 6;
+
+         String resultado = "";
+         if(a==b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";
+
+        // ULTILIZANDO OPERADOR TERNÁRIO
+        int a, b;
+
+         a = 5;
+         b = 6;
+
+         String resultado = a == b ? "verdadeiro" : "falso";
+         
+
+         //OPERADORES RELACIONAIS
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        String nomeUm = "Afonso";
+        String nomeDois = "Afonso";
+
+        System.out.println(nomeUm.equals(nomeDois));// O método "equals" compara conteúdos(forma mais adequada para comparar objetos)
+
+
+         boolean simNao = numero1 == numero2;
+
+        if(numero1 < numero2){
+            
+            System.out.println("A nossa condição é verdadeira!");
+        }
+
+        System.out.println("numeroUm é igual a numeroDois? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numeroUm é diferente de numeroDois? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numeroUm é maior que numeroDois? " + simNao);
+        
+        //OPERADORES LÓGICOS
+        boolean condicao1 = true;
+
+        boolean condicao2 = true;
+
+        if(condicao1 && condicao2){
+            System.out.println("As duas condições são verdadeiras!");
+        
+        }
+        if(condicao1 || condicao2){
+            System.out.println("Uma das condições é verdadeira!");
+        }
+        if(condicao1 && (7 > 4)){
+            System.out.println("As duas condições são verdadeiras!");
+        
+        }
+
+        System.out.println("Fim");
+        */
+        
+
+        
+
+         
+
+        
+
+
+        }
 }
